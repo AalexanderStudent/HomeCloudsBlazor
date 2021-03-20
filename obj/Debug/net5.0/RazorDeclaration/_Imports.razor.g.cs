fca8afb99c3fa2f9d18fc35cf80008f005d5ef44
Ilya <thatlazyseal@gmail.com>
2021-03-20 23:00:34 +0300

    added login page

--- obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,71 +13,78 @@ namespace HomeCLoudsBlazor
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 1 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 2 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 3 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 4 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 5 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 6 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 7 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 8 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
+#line 9 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
 using HomeCLoudsBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\dev\c#\HomeCloudsBlazor\HomeCLoudsBlazor\_Imports.razor"
-using HomeCLoudsBlazor.Shared;
+#line 10 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
+using MudBlazor;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "C:\Users\docuc\RiderProjects\HomeCloudsBlazor\_Imports.razor"
+using HomeCLoudsBlazor.Components;
 
 #line default
 #line hidden
